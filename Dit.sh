aea395be0a1f2f27a70125ae49ef6d84b18e9bb5
Aditia18 <39555780+Aditia18@users.noreply.github.com>
2019-10-20 17:15:04 +0700

    Add files via upload

---
+++ Dit.sh
@@ -0,0 +1,166 @@
+clear
+figlet -f bigmono9 "InsideHeartz Spam" | lolcat
+echo "\033[38;1m ------------------------------------------------------------------"|lolcat
+echo "\033[31;1m  "
+echo "\033[36;1m" "                    AUTHOR : Aditia Alfiansyah"
+echo "\033[36;1m" "                    		TERSAKITI CREW"
+echo "\033[35;1m #Budidayakan Spam Mantan#"
+echo "\033[34;1m" "WhatsApp : +1 (313)2617183"
+echo "\033[34;1m" "LAWAK LO BADUT"
+echo "\033[38;1m ------------------------------------------------------------------"|lolcat
+date | lolcat
+echo ""
+     echo "\033[34;1m"" SPAM SMS Yang Tersedia "
+     echo "\033[33;1m""    (01)JDID "
+     echo "\033[32;1m""    (02)TELKOMSEL "
+     echo "\033[33;1m""    (03)PHD"
+     echo "\033[32;1m""    (04)TOKPED" 
+     echo "\033[33;1m""    (05)Lazada "
+     echo "\033[32;1m""    (06)OYO "
+     echo "\033[33;1m""    (07)WHISKAS "
+     echo "\033[32;1m""    (08)MATAHARI MALL "
+     echo "\033[33;1m""    (09)ZIPAY "
+     echo "\033[32;1m""    (10)PHDv2 "
+     echo "\033[33;1m""    (11)NXSMS "
+     echo "\033[32;1m""    (12)TOKOPEDIA "
+     echo "\033[33;1m""    (13)KFC "
+     echo "\033[32;1m""    (14)HOOQ "
+     echo "\033[33;1m""    (15)CALL "
+     echo "\033[34;1m""    (00)INFO "
+     echo "\033[34;1m""    (99)Keluar "
+echo "\033[33;1m"" pilih nomer nya "
+read ezz 
+
+if [ $ezz = 1 ] || [ $ezz = 01 ]
+then
+clear
+toilet -f lean "JDID"
+php jdid.php
+sh Dit.sh
+fi
+
+if [ $ezz = 02 ] || [ $ezz = 2 ]
+then
+clear
+toilet -f  lean  "TELKOMSEL"
+php telkomsel.php
+sh Dit.sh
+fi
+
+if [ $ezz = 03 ] || [ $ezz = 3 ]
+then
+clear 
+toilet -f lean "PHD"
+php phd.php
+sh Dit.sh
+fi
+
+if [ $ezz = 04 ] || [ $ezz = 4 ]
+then
+clear
+toilet -f lean "TOKPED"
+php tokped.php
+sh Dit.sh
+fi
+
+if [ $ezz = 05 ] || [ $ezz = 5 ]
+then
+clear
+python2 lazada.py
+sh Dit.sh
+fi
+
+if [ $ezz = 06 ] || [ $ezz = 6 ]
+then
+clear
+toilet -f lean "OYO"
+php oyo.php
+sh Dit.sh
+fi
+
+if [ $ezz = 07 ] || [ $ezz = 7 ]
+then
+clear
+toilet -f lean "WHISKAS"
+php Whiskas.php
+sh Dit.sh
+fi
+
+if [ $ezz = 08 ] || [ $ezz = 8 ]
+then
+clear
+toilet -f lean "MATAHARI MALL"
+php MatahariMall.php
+sh Dit.sh
+fi
+
+if [ $ezz = 09 ] || [ $ezz = 9 ]
+then
+clear
+toilet -f lean "ZIPAY"
+php Zipay.php
+sh Dit.sh
+fi
+
+if [ $ezz = 10 ] || [ $ezz = 10 ]
+then
+clear
+toilet -f lean "PHDv2"
+php PHDv2.php
+sh Dit.sh
+fi
+
+if [ $ezz = 11 ] || [ $ezz = 11 ]
+then
+clear
+toilet -f lean "NXSMS"
+php NXsms.php
+sh Dit.sh
+fi
+
+if [ $ezz = 12 ] || [ $ezz = 12 ]
+then
+clear
+toilet -f lean "TOKOPEDIA"
+php tokopedia.php
+sh Dit.sh
+fi
+
+if [ $ezz = 13 ] || [ $ezz = 13 ]
+then
+clear
+toilet -f lean "KFC"
+php kfc.php
+sh Dit.sh
+fi
+
+if [ $ezz = 14 ] || [ $ezz = 14 ]
+then
+clear
+toilet -f lean "HOOQ"
+php hooq.php
+sh Dit.sh
+fi
+
+if [ $ezz = 15 ] || [ $ezz = 15 ]
+then
+clear
+toilet -f lean "CALL"
+php Call.php
+sh Dit.sh
+fi
+if [ $ezz = 00 ]
+then 
+clear
+toilet -f lean  "INFO"
+echo "Dibuat oleh "
+echo "Aditia Alfiansyah | Inside Heartz"
+echo "Whatsapp Me : "
+echo "+1(313)2617183"
+fi
+
+if [ $ezz = 99 ]
+then
+echo "semoga hari mu menyenangkan"
+echo " TerimaKasih "
+fi
